732bf5a0b0f62bb91e1c328ffcdb2dca87ea1159
Jitesh Sinha <jiteshmca2020@gmail.com>
2020-12-02 22:22:16 +0530

    [Update] Perform redirection based on method type
    Signed-off-by: Jitesh Sinha <jiteshmca2020@gmail.com>

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,26 @@
+//perform directly get-request
 var inp = document.getElementById('redirect-url');
 var redirectURL = document.getElementById('redirect-link');
 
 inp.addEventListener('input', function() {
 	var ref = inp.value;
 	redirectURL.href = ref + '';
+});
+
+//perform request based on method type
+var methodType = document.getElementById('method-type');
+var formRequest = document.getElementById('form-request');
+
+methodType.addEventListener('input', function() {
+	var name = methodType.value;
+	formRequest.method = name + '';
+});
+
+
+var input = document.getElementById('request-url');
+var requestURL = document.getElementById('form-request');
+
+input.addEventListener('input', function() {
+	var ref = input.value;
+	requestURL.action = ref + '';
 });
